a12b6e79d82fab6151ece1432f3691dede7f84d4
khanmahfuj34 <mahfuj34@gmail.com>
2026-02-11 16:01:53 +0600

    Update project files

--- script/index.js
+++ script/index.js
@@ -12,13 +12,33 @@ const loadLevelWord = (level_no) => {
         .then(res => res.json())
         .then(data => displayLevelWord(data.data))
 }
+
+
+// {
+//     "id": 5,
+//     "level": 1,
+//     "word": "Eager",
+//     "meaning": "আগ্রহী",
+//     "pronunciation": "ইগার"
+// }
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById("word-container");
+    console.log(words);
     wordContainer.innerHTML = "";
     words.forEach(word => {
         const wordDiv = document.createElement("div");
         wordDiv.innerHTML = `
-        <p>hello</p>
+         <div class="bg-white rounded-xl shadow-sm text-center py-6 md:py-8 px-3 md:px-4 space-y-3 h-full flex flex-col justify-center">
+            <h2 class="font-bold text-lg md:text-xl lg:text-2xl">${word.word}</h2>
+            <p class="font-semibold text-sm md:text-base">Meaning /Pronounciation</p>
+            <div class="font-bangla text-base md:text-lg lg:text-xl font-semibold">"${word.meaning} / ${word.pronunciation} "</div>
+            <div class="flex justify-between items-center gap-2 mt-4">
+                <button class="btn btn-sm md:btn-md bg-[#1A91FF10] hover:bg-[#1A91FF60]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn btn-sm md:btn-md bg-[#1A91FF10] hover:bg-[#1A91FF60]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+
+
+        </div>
 
         `;
         wordContainer.append(wordDiv);
@@ -32,7 +52,7 @@ const displayLessons = (lessons) => {
         // console.log(lesson);
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-                            <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+                            <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-sm md:btn-md btn-outline btn-primary text-xs md:text-base">
                             <i class="fa-solid fa-arrow-right-from-bracket"></i>Lesson -${lesson.level_no}</button>
         `
         levelContainer.append(btnDiv);
